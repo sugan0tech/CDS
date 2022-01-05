78b11310d7cd63a358f41a98de4117842ff7616d
sugan0tech <sugankpms@gmail.com>
2022-01-05 12:07:26 +0530

    updated upto jan 5

--- data structures/my code/temp.c
+++ data structures/my code/temp.c
@@ -1,62 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-struct Node
+#include <stdio.h>
+int main()
 {
-    int data;
-    struct Node *next;
-};
+    int t;
+    scanf("%d", &t);
 
-struct Node *Top, *temp;
-
-void push();
-void pop();
-void display();
-
-void main()
-{
-    push();
-    push();
-    push();
-    push();
-    pop();
-    display();
-}
-
-void push()
-{
-    int n;
-    struct Node *new;
-    new = (struct Node *)malloc(sizeof(struct Node));
-    printf("Enter the num :");
-    scanf("%d", &n);
-    new->data = n;
-    new->next = NULL;
-    if (Top == NULL)
+    for (int i = 0; i < t; i++)
     {
-        Top = new;
-        Top->next = NULL;
+        int n, last = 0, first = 0;
+        ;
+        scanf("%d", &n);
+        last = last + n % 10;
+        while (n > 0)
+        {
+            first = n % 10;
+            n = n / 10;
+        }
+        first = first + last;
+        printf("%d", first);
     }
-    else 
-    {
-        new->next = Top;
-        Top = new;
-    }
-    printf("pushed \n");
-}
-
-void pop()
-{
-    temp = Top;
-    Top = temp->next;
-}
-void display()
-{
-    temp = Top;
-    while(temp != NULL)
-    {
-        printf("\n%d\n", temp->data);
-        temp = temp->next;
-    }
-    printf("Done with printing \n");
+    return 0;
 }
